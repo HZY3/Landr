a5c9cfddf748ec83bba94e4f6dd56c9c75a7e683
KSHTJ <66157989+HZY3@users.noreply.github.com>
2023-02-04 09:52:58 +0530

    Update Home.jsx

--- src/components/Navbar/Home.jsx
+++ src/components/Navbar/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import './Login.css'
 
 
-
-// import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-
-
 function Home() {
     return (
         <div className='Navbar-HomeContent'>
-            <h1>Next Gen Solution For Land Registration <br/>Using Blockchain</h1>
+            <h2 className='no-margin-sticky Top-Main-title'>Welcome to Landr</h2>
+            <h1 className='no-margin-sticky Main-title'>Next Gen Solution For Land Registration Using Blockchain</h1>
+            <h3 className='no-margin-sticky Home-Intro'>   
+                Land Registration and Trading  have never been so easy and secure.<br/>
+                We believe that blockchain technology in conjunction with Aadhar card 
+                would be an excellent tool for increasing transparency in government 
+                functions, eliminating fraud and corruption, and enhancing socio economic advantages. 
+            </h3>
         </div>
     )
 }
